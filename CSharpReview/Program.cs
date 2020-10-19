6e9db461fc518fd750040604695e3e79c9024e53
Kenji <kenji.au9@gmail.com>
2020-10-19 09:52:55 -0600

    Console input and output variables

--- CSharpReview/Program.cs
+++ CSharpReview/Program.cs
@@ -4,9 +4,18 @@ namespace CSharpReview
 {
   class Program
   {
+    // Entry point of programs in C#
     static void Main(string[] args)
     {
+      // Declaration of variable.
+      string name;
+
+      // Input from the consle. Console.ReadLine()
+      name = Console.ReadLine();
+
+      // Output to the console.
       Console.WriteLine("Hello World! What a nice day!");
     }
   }
+  // Exit point of programs in C#.
 }
